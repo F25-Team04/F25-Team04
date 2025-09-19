cfa685baa74fb94c74e35b3e42a52cd132f91c63
Jarrrw <jarrettwilson09@att.net>
2025-09-19 12:46:27 -0400

    Dropdown menu is working with the database

--- pages/signup/apply.js
+++ pages/signup/apply.js
@@ -4,29 +4,23 @@ window.onload = function() {
     // for the user to select from when applying to an organization
     function generateOrgs(orgs) {
         const dropdown = document.getElementById("dropdown")
-        console.log(orgs)
-        // orgs.forEach(org => {
-        //     const option = document.createElement("option");
-        //     option.value = org;
-        //     option.textContent = org;
-        //     dropdown.appendChild(option);
-        // });
+        orgs.forEach(org => {
+            const option = document.createElement("option");
+            option.value = org;
+            option.textContent = org;
+            dropdown.appendChild(option);
+        });
     }
 
     // Gets the organizations registered that are already registered to the Server
     fetch("https://5ynirur3b5.execute-api.us-east-2.amazonaws.com/dev/organizations")
-        .then(response => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok " + response.statusText);
-        }
-        generateOrgs(response)
-    })
-    .then(data => {
-        console.log(data); // handle the JSON data
-    })
-    .catch(error => {
-        console.error("There was a problem with the fetch operation:", error);
-    });
+        .then(response => response.json())
+        .then(orgs => {
+            generateOrgs(orgs); // handle the JSON data
+        })
+        .catch(error => {
+            console.error("There was a problem with the fetch operation:", error);
+        });
 
     // // Posts the signup form to the API
     // fetch("https://api.example.com/data")
