11c1c5e043db861f79c944e4b8b7257abd917eb9
Samuel Bangert <bangertsam@protonmail.com>
2025-11-11 14:27:46 -0500

    Change navbar to say dashboard instead of home

--- pages/AdminCreateSponsorOrg/AdminCreateSponsorOrg.js
+++ pages/AdminCreateSponsorOrg/AdminCreateSponsorOrg.js
@@ -6,7 +6,7 @@ window.onload = function () {
   const li = document.createElement("li");
   const link = document.createElement("a");
   link.href = "../AdminHomepage/AdminHome.html?id=" + USER_ID;
-  link.textContent = "Home";
+  link.textContent = "Dashboard";
   li.appendChild(link);
   const create = document.createElement("a");
   create.href = "../AdminCreateSponsor/AdminCreateSponsor.html?id=" + USER_ID;
