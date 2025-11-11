23b0d060260384fcc602483134a37cd7f1acad31
Samuel Bangert <bangertsam@protonmail.com>
2025-11-11 10:22:56 -0500

    Add page for sponsor organization creation

---
+++ pages/AdminCreateSponsorOrg/AdminCreateSponsorOrg.js
@@ -0,0 +1,54 @@
+const params = new URLSearchParams(window.location.search);
+const USER_ID = params.get("id");
+
+window.onload = function () {
+  var list = this.document.getElementById("links");
+  const li = document.createElement("li");
+  const link = document.createElement("a");
+  link.href = "../AdminHomepage/AdminHome.html?id=" + USER_ID;
+  link.textContent = "Home";
+  li.appendChild(link);
+  const create = document.createElement("a");
+  create.href = "../AdminCreateSponsor/AdminCreateSponsor.html?id=" + USER_ID;
+  create.textContent = "Create Sponsor";
+  const create_org = document.createElement("a");
+  create_org.href = "../AdminCreateSponsorOrg/AdminCreateSponsorOrg.html?id=" + USER_ID;
+  create_org.textContent = "Create Organization";
+  li.appendChild(create);
+  li.appendChild(create_org);
+  list.appendChild(li);
+
+  document
+    .getElementById("create")
+    .addEventListener("submit", async function (event) {
+      event.preventDefault(); // stop normal form submission
+
+      // Gather form data
+      const form = event.target;
+      const formData = new FormData(form);
+      try {
+        // Send POST request
+        const response = await fetch(
+          "https://ozbssob4k2.execute-api.us-east-1.amazonaws.com/dev/sponsor",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json", // IMPORTANT
+            },
+            body: JSON.stringify(formData),
+          }
+        );
+        if (response.ok) {
+          const result = await response.json();
+
+          if (result.success == false) {
+            alert(result.message);
+          } else if (result.success == true) {
+            GetUser(result.message);
+          }
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    });
+};
